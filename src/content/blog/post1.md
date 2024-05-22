820ff1ac0f45a176cc800960c9c691bc5d6be5e3
mike connor <mike@mikeconnor.tech>
2024-05-22 13:57:38 -0500

    fix and format

--- src/content/blog/post1.md
+++ src/content/blog/post1.md
@@ -1,9 +1,9 @@
 ---
-title: "Demo Post 1"
-description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-pubDate: "Sep 10 2022"
-heroImage: "/post_img.webp"
-tags: ["tokio"]
+title: 'Demo Post 1'
+description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+pubDate: 'Sep 10 2022'
+heroImage: '/post_img.webp'
+tags: ['tokio']
 ---
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,37 +1,46 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection } from 'astro:content';
 
 const blogSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.string().optional(),
+  heroImage: z.string(),
+  badge: z.string().optional(),
+  tags: z
+    .array(z.string())
+    .refine((items) => new Set(items).size === items.length, {
+      message: 'tags must be unique',
+    })
+    .optional(),
 });
 
 const storeSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
-    updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing: z.string().optional(),
-    badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
-    heroImage: z.string().optional(),
+  title: z.string(),
+  description: z.string(),
+  custom_link_label: z.string(),
+  custom_link: z.string().optional(),
+  updatedDate: z.coerce.date(),
+  pricing: z.string().optional(),
+  oldPricing: z.string().optional(),
+  badge: z.string().optional(),
+  checkoutUrl: z.string().optional(),
+  heroImage: z.string(),
 });
 
 export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 
-const blogCollection = defineCollection({ schema: blogSchema, type: 'content' });
-const storeCollection = defineCollection({ schema: storeSchema ,  type: 'content'});
+const blogCollection = defineCollection({
+  schema: blogSchema,
+  type: 'content',
+});
+const storeCollection = defineCollection({
+  schema: storeSchema,
+  type: 'content',
+});
 
 export const collections = {
-    'blog': blogCollection,
-    'store': storeCollection
-}
+  blog: blogCollection,
+  store: storeCollection,
+};
