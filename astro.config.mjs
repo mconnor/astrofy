1ec2bb8a217ae5836ea123461c350324a74a64f9
mike connor <mike@mikeconnor.tech>
2024-10-06 19:24:50 -0500

    go back

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+// import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
   output: 'static',
   integrations: [mdx(), sitemap(), tailwind()],
+  // adapter: vercel(),
 });
