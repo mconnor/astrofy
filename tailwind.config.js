5921a74f225375315cfebdd9735c076d39cbcce4
mike connor <mike@mikeconnor.tech>
2025-05-03 15:18:45 -0500

    tailwind3 proper setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
+import typo from '@tailwindcss/typography';
+export default {
+  content: ['./src/**/*.{astro,html,js, md,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: [typo, daisyui],
   daisyui: {
     themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: 'dark', // name of one of the included themes for dark mode
